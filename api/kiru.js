3b370d089651d01861569d06517e03f6f52f27ab
nanashi <jfabro3124@gmail.com>
2021-05-04 03:40:26 +0800

    class Kiru refactored object creation code

--- api/kiru.js
+++ api/kiru.js
@@ -11,8 +11,10 @@ class Kiru {
 		console.log(`Navigation to ${this._url}...`);
 		await page.goto(this._url);
 
-		let { directory, next } = this._selectors;
-		let [mangaListItems, directLink] = directory;
+		const { directory, next } = this._selectors;
+		const [mangaListItems, directLink] = directory;
+		const { title, image, status, latest, description } = this._selectors;
+		const [chapterList, chapterLink] = latest;
 
 		let scrapedData = [];
 
@@ -37,66 +39,69 @@ class Kiru {
 					let newPage = await browser.newPage();
 					await newPage.goto(link);
 
-					//creates an object
-					await createObj(dataObj, link);
+					dataObj['link'] = link;
+
+					dataObj['title'] = await newPage.$eval(
+						`${title}`,
+						(title) => title.textContent
+					);
+
+					dataObj['image'] = await newPage.$eval(`${image}`, (img) => img.src);
+
+					dataObj['status'] = await newPage.$eval(
+						`${status}`,
+						(status) => status.textContent
+					);
+
+					dataObj['latest'] = await newPage.evaluate(
+						(chapterList, chapterLink) => {
+							const list = document.querySelector(`${chapterList}`);
+							const direct = list.querySelector(`${chapterLink}`).textContent;
+							let currentChapter;
+
+							if (direct.includes(':')) {
+								let array = direct.toString().split(':');
+								currentChapter = array[0];
+								return currentChapter;
+							}
+
+							return direct.toString();
+						},
+						chapterList,
+						chapterLink
+					);
+
+					dataObj['latestLink'] = await newPage.evaluate(
+						(chapterList, chapterLink) => {
+							const latest = document.querySelector(`${chapterList}`);
+							const link = latest.querySelector(`${chapterLink}`);
+
+							return link.href;
+						},
+						chapterList,
+						chapterLink
+					);
+
+					dataObj['description'] = await newPage.$eval(
+						`${description}`,
+						(p) => p.textContent
+					);
 
 					resolve(dataObj);
 					await newPage.close();
 				});
 
-			let createObj = async (object, link) => {
-				let { title, image, status, latest, description } = this._selectors;
-				let [chapterList, chapterLink] = latest;
-
-				object['link'] = link;
-
-				object['title'] = await newPage.$x(
-					`${title}`,
-					(title) => title.textContent
-				);
-
-				object['image'] = await newPage.$x(`${image}`, (img) => img.src);
-
-				object['status'] = await newPage.$x(`${status}`, status.textContent);
-
-				object['latest'] = await newPage.$x(`${chapterList}`, (table) => {
-					let currentChapter;
-					let textContent = table.firstElementChild.querySelector(
-						`${chapterLink}`
-					).textContent;
-
-					if (textContent.includes(':')) {
-						let chapter = textContent.split(':');
-						currentChapter = chapter[0];
-						return currentChapter;
-					}
-
-					currentChapter = textContent;
-					return currentChapter;
-				});
-
-				object['latestLink'] = await newPage.$x(
-					`${chapterList}`,
-					(table) =>
-						table.firstElementChild.querySelector(`${chapterLink}`).href
-				);
-
-				object['description'] = await newPage.$x(
-					`${description}`,
-					(p) => p.textContent
-				);
-			};
-
 			//loop all manga links inside mangalist url
 			for (let link in urls) {
 				let currentPageData = await pagePromise(urls[link]);
 				scrapedData.push(currentPageData);
+				console.log(currentPageData);
 			}
 
 			//Use Recursion to traverse pagination
 			let nextButtonExist = false;
 			try {
-				const nextButton = await page.$x(`${next}`, (a) => a.textContent);
+				const nextButton = await page.$eval(`${next}`, (a) => a.textContent);
 
 				nextButtonExist = true;
 			} catch (err) {
--- api/sites/special.js
+++ api/sites/special.js
@@ -6,11 +6,11 @@ class MangaKatana extends Kiru {
 		url = MangaKatana.search(keyword),
 		selectors = {
 			directory: ['#book_list > div', 'h3 > a'],
-			title: '//*[@id="single_book"]/div[2]/div/h1',
-			image: '//*[@id="single_book"]/div[1]/div/img',
-			status: '//*[@id="single_book"]/div[2]/div/ul/li[4]/div[2]',
-			latest: ['//*[@id="single_book"]/div[7]/table/tbody', 'chapter > a'],
-			description: '//*[@id="single_book"]/div[3]/p',
+			title: '.info > h1',
+			image: '.cover img',
+			status: '.d-row-small > .status',
+			latest: [`tbody`, `a`],
+			description: '.summary > p',
 			next: '.next.page-numbers',
 		}
 	) {
@@ -24,29 +24,5 @@ class MangaKatana extends Kiru {
 		return url;
 	}
 }
-
-// function mangaKatana(keyword) {
-// 	return {
-
-// this._selectors = {   depending sa site pero since mangakatana is main
-// 	* 		directory : [manga list urls, link path],
-// 	*		title: title path
-// 			image: img path
-// 		   status: status path
-// 		   latest: latest path
-// 		   description: description path
-// 		   next: next button path
-// 	*
-// 	* }
-// 		keyword,
-
-// 		searchKeywordUrl() {
-// 			let query = keyword.split(' ').join('+');
-// 			let url = `https://mangakatana.com/?search=${query}`;
-
-// 			return url;
-// 		},
-// 	};
-// }
 
 module.exports = MangaKatana;
