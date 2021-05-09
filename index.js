3fed1f42cece1d47454b325298b8c580099431b1
nanashi <jfabro3124@gmail.com>
2021-05-09 17:22:00 +0800

    uninstalled express.js

--- index.js
+++ index.js
@@ -1,41 +1,4 @@
-const express = require('express');
-const app = express();
-const port = 3000;
-
 const search = require('./api/pageScraper');
-
-app.use(express.json());
-app.use(function (req, res, next) {
-	res.header('Access-Control-Allow-Origin', '*'); // disabled for security on local
-	res.header('Access-Control-Allow-Headers', 'Content-Type');
-	next();
-});
-// http://localhost:3000/manga?source=mangakatana-manganelo&keyword=grand_blue
-app.get('/manga', (req, res) => {
-	let source = req.query.source;
-	let keyword = req.query.keyword;
-
-	function formatSources(src) {
-		return src.split('-');
-	}
-
-	function formatKeyword(keyword) {
-		return keyword.replace(/_/g, ' ').toString();
-	}
-
-	let data = search.launchScraper(
-		formatKeyword(keyword),
-		formatSources(source)
-	);
-
-	res.header('Content-Type', 'application/json');
-	res.json(data);
-	res.status(500).json({ error: 'message' });
-});
-
-app.listen(port, () => {
-	console.log(`API listening at http://localhost:${port}`);
-});
 
 //Test searches
 // search.launchScraper('le');
@@ -45,3 +8,18 @@ app.listen(port, () => {
 // search.launchScraper(`Path of the Shaman`);
 
 module.exports = app;
+
+// let source = req.query.source;
+// let keyword = req.query.keyword;
+// function formatSources(src) {
+// 	return src.split('-');
+// }
+// function formatKeyword(keyword) {
+// 	return keyword.replace(/_/g, ' ').toString();
+// }
+// let data = search.launchScraper(
+// 	formatKeyword(keyword),
+// 	formatSources(source)
+// );
+// res.header('Content-Type', 'application/json');
+// res.send({ name: 'john' });
