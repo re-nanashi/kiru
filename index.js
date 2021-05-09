8c8948d1adb77c4618caed2f36df4794d7b148f2
nanashi <jfabro3124@gmail.com>
2021-05-09 12:52:47 +0800

    created api endpoint

--- index.js
+++ index.js
@@ -4,24 +4,41 @@ const port = 3000;
 
 const search = require('./api/pageScraper');
 
-// app.use(express.json());
-// app.use(function (req, res, next) {
-// 	res.header('Access-Control-Allow-Origin', '*'); // disabled for security on local
-// 	res.header('Access-Control-Allow-Headers', 'Content-Type');
-// 	next();
-// });
+app.use(express.json());
+app.use(function (req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*'); // disabled for security on local
+	res.header('Access-Control-Allow-Headers', 'Content-Type');
+	next();
+});
+//example www.api.com/manga/?source=mangakata-manganelo-asurascans&query=karakai_jouzu
+app.get('/search', (req, res) => {
+	let source = req.query.source.toLowerCase();
+	let keyword = req.query.keyword.toLowerCase();
 
-// app.get('/', (req, res) => {
-// 	res.send('Hello World!');
-// });
+	function formatSources(src) {
+		return src.split('-');
+	}
 
-// app.listen(port, () => {
-// 	console.log(`Example app listening at http://localhost:${port}`);
-// });
+	function formatKeyword(keyword) {
+		return keyword.replace(/_/g, ' ');
+	}
+
+	search.launchScraper(formatKeyword(keyword), formatSources(source));
+	const data = require('./data.json');
+
+	res.header('Content-Type', 'application/json');
+	res.send(data);
+});
+
+app.listen(port, () => {
+	console.log(`Example app listening at http://localhost:${port}`);
+});
 
 //Test searches
 // search.launchScraper('le');
-// search.launchScraper('gin no saji');
-search.launchScraper('karakai jouzu', ['mangakatana', 'manganelo']);
+// search.launchScraper('grand blue', ['mangakatana', 'manganelo']);
+
 // search.launchScraper('legend');
 // search.launchScraper(`Path of the Shaman`);
+
+module.exports = app;
