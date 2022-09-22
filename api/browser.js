de8e8490f6c4baa17c0ec668914010f507d2a6ae
re-nanashi <jfabro3124@gmail.com>
2022-09-22 18:43:53 +0800

    Create browser class

--- api/browser.js
+++ api/browser.js
@@ -1,39 +1,50 @@
-/** @brief Start browser instance.
+/** @brief Brower class.
  *
- *  @param void
+ *  Creates a browser instance for executing the scraper.
+ *
+ *  @constructor void
  *  @return returns a promise which resolves to browser instance
  */
-async function startBrowser() {
-    // Initialize parameters
-    let chrome = {};
-    let puppeteer;
+class Browser {
+    constructor() {
+        this.chrome = {};
+        this.puppeteer;
+        this.browser_instance;
 
-    if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
-    	// running on the Vercel platform.
-    	chrome = require('chrome-aws-lambda');
-    	puppeteer = require('puppeteer-core');
-    } else {
-    	// running locally.
-    	puppeteer = require('puppeteer');
+        // Define the chrome and puppeteer variable
+        if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+        	// running on the Vercel platform.
+        	chrome = require('chrome-aws-lambda');
+        	puppeteer = require('puppeteer-core');
+        } else {
+        	// running locally.
+        	puppeteer = require('puppeteer');
+        }
     }
 
-    // Create browser instance
-	let browser;
-	try {
-		browser = await puppeteer.launch({
-			args: chrome.args,
-			defaultViewport: chrome.defaultViewport,
-			executablePath: await chrome.executablePath,
-			headless: true,
-			ignoreHTTPSErrors: true,
-		});
-	} catch (err) {
-		console.log('Could not create a browser instance => :', err);
-	}
+    /** @brief Starts the browser instance.
+     *
+     *  @constructor void
+     *  @return returns a promise which resolves to browser instance
+     */
+    async start() {
+        // Launch browser instance
+	    try {
+	    	browser = await puppeteer.launch({
+	    		args: chrome.args,
+	    		defaultViewport: chrome.defaultViewport,
+	    		executablePath: await chrome.executablePath,
+	    		headless: true,
+	    		ignoreHTTPSErrors: true,
+	    	});
+	    } catch (err) {
+	    	console.log('Could not create a browser instance => :', err);
+	    }
 
-	return browser;
+	    return browser;
+    }
 }
 
 module.exports = {
-	startBrowser,
+	Browser,
 };
