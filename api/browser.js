8fadc27b37d3d0c131dec596fd07bf58cd2d9ed1
re-nanashi <jfabro3124@gmail.com>
2022-09-23 10:08:45 +0800

    refactor browser file by creating a class

--- api/browser.js
+++ api/browser.js
@@ -1,4 +1,4 @@
-/** @brief Brower class.
+/** @brief Browser class.
  *
  *  Creates a browser instance for executing the scraper.
  *
@@ -6,45 +6,43 @@
  *  @return returns a promise which resolves to browser instance
  */
 class Browser {
-    constructor() {
-        this.chrome = {};
-        this.puppeteer;
-        this.browser_instance;
+	constructor() {
+		this.chrome = {};
+		this.puppeteer;
+		this.browser_instance;
 
-        // Define the chrome and puppeteer variable
-        if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
-        	// running on the Vercel platform.
-        	chrome = require('chrome-aws-lambda');
-        	puppeteer = require('puppeteer-core');
-        } else {
-        	// running locally.
-        	puppeteer = require('puppeteer');
-        }
-    }
+		// Define the chrome and puppeteer variable
+		if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+			// running on the Vercel platform.
+			chrome = require("chrome-aws-lambda");
+			puppeteer = require("puppeteer-core");
+		} else {
+			// running locally.
+			puppeteer = require("puppeteer");
+		}
+	}
 
-    /** @brief Starts the browser instance.
-     *
-     *  @constructor void
-     *  @return returns a promise which resolves to browser instance
-     */
-    async start() {
-        // Launch browser instance
-	    try {
-	    	browser_instance = await puppeteer.launch({
-	    		args: chrome.args,
-	    		defaultViewport: chrome.defaultViewport,
-	    		executablePath: await chrome.executablePath,
-	    		headless: true,
-	    		ignoreHTTPSErrors: true,
-	    	});
-	    } catch (err) {
-	    	console.log('Could not create a browser instance => :', err);
-	    }
+	/** @brief Starts the browser instance.
+	 *
+	 *  @constructor void
+	 *  @return returns a promise which resolves to browser instance
+	 */
+	async start() {
+		// Launch browser instance
+		try {
+			browser_instance = await puppeteer.launch({
+				args: chrome.args,
+				defaultViewport: chrome.defaultViewport,
+				executablePath: await chrome.executablePath,
+				headless: true,
+				ignoreHTTPSErrors: true,
+			});
+		} catch (err) {
+			console.log("Could not create a browser instance => :", err);
+		}
 
-	    return browser_instance;
-    }
+		return browser_instance;
+	}
 }
 
-module.exports = {
-	Browser,
-};
+module.exports = Browser;
